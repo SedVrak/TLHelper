0a92a9da4e87184c720050af68378d7b14752af8
SedVrak <mishased3k@ukr.net>
2019-01-03 23:41:32 +0200

    get messages from chats

--- TLHelper/Program.cs
+++ TLHelper/Program.cs
@@ -89,27 +89,65 @@ namespace TLHelper
                 var hash = await client.SendCodeRequestAsync(phoneNumber);
 
                 Console.WriteLine("Enter Login code:");
-                string code = Console.ReadLine();
+                string codeLogin = Console.ReadLine();
                 Console.WriteLine();
 
-                var user = await client.MakeAuthAsync(phoneNumber, hash, code);
+                var user = await client.MakeAuthAsync(phoneNumber, hash, codeLogin);
             }
 
-            var found = await client.SearchUserAsync("msg_below", 1);
-            var u = found.Users.ToList().OfType<TLUser>().FirstOrDefault();
-            
-            var peer = new TLInputPeerUser() { UserId = u.Id, AccessHash = (long)u.AccessHash };
+            //var found = await client.SearchUserAsync("msg_below", 1);
+            //var u = found.Users.ToList().OfType<TLUser>().FirstOrDefault();
+
+            //var peer = new TLInputPeerUser() { UserId = u.Id, AccessHash = (long)u.AccessHash };
             //Надсилаю повідомлення
-            Console.WriteLine("Enter ur test message: ");
-            string a = Console.ReadLine();
-            await client.SendMessageAsync(peer, a);
+            //Console.WriteLine("Enter ur test message: ");
+            //string a = Console.ReadLine();
+            //await client.SendMessageAsync(peer, a);
 
 
-            //Метод який "слухає" повідомлення
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+            while (true)
+            { 
+                foreach (var dia in dialogs.Dialogs.ToList())
+                {
+                    if (dia.UnreadCount < 1) continue;
 
+                    if (dia.Peer is TLPeerUser)
+                    {
+                        var peer = dia.Peer as TLPeerUser;
+                        var chat = dialogs.Users.ToList().OfType<TLUser>().First(x => x.Id == peer.UserId);
+                        var target = new TLInputPeerUser { UserId = chat.Id, AccessHash = chat.AccessHash ?? default(long) };
+                        var hist = await client.GetHistoryAsync(target, 0, -1, dia.UnreadCount);
+                        int firstMessage = 0;
 
+                        if (hist is TLMessagesSlice)
+                        {
+                            int index = 0;
+                            foreach (var m in ((TLMessagesSlice)hist).Messages.ToList())
+                            {
+                                TLMessage msg = m as TLMessage;
 
+                                if (index == 0) firstMessage = msg.Id;
 
+                                Console.WriteLine("{0} {1} {2}", msg.Id, msg.Message, msg.FromId);
+                            }
+                        }
+                        else if (hist is TLMessages)
+                        {
+                            int index = 0;
+                            foreach (var m in ((TLMessages)hist).Messages.ToList())
+                            {
+                                TLMessage msg = m as TLMessage;
+
+                                if (index == 0) firstMessage = msg.Id;
+
+                                Console.WriteLine("{0} {1} {2}", msg.Id, msg.Message, msg.FromId);
+                            }
+                        }
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
         }
 
     }
